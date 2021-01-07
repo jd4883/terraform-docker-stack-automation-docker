6df6e19484a3a9ab01e1e51fab052d2fb112b135
Condoriano <dresdale.jacob@gmail.com>
2021-01-06 18:44:18 -0800

    Update resources.tf
    added in a few extra options

--- resources.tf
+++ resources.tf
@@ -42,6 +42,10 @@ resource "docker_container" "container" {
   tmpfs             = try(tomap(each.value.tmpfs), {})
   entrypoint        = try(tolist(each.value.Entrypoint), [])
   network_mode      = try("container:${each.value.networks.vpn}", "default")
+  working_dir       = try(each.value.working_dir, "")
+  user              = try(each.value.user, "")
+  upload            = try(each.value.upload, [])
+  ulimit            = try(each.value.ulimit, [])
   dynamic "ports" {
     for_each = try(try(each.value.networks.vpn, "default") == "default" ? tolist(each.value.ports) : [], [])
     content {
