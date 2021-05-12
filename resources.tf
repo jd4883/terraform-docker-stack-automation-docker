3cfa3aa1164828c624001f0c0bcfbecb76c85e3c
Jacob Dresdale <15719920+jd4883@users.noreply.github.com>
2021-05-11 22:43:10 -0700

    testing updated forward_auth

--- resources.tf
+++ resources.tf
@@ -78,8 +78,7 @@ resource "docker_container" "container" {
         "traefik.enable" : try(each.value.public_dns, true),
       },
       tobool(try(each.value.public_dns, true)) ? merge(tobool(try(each.value.okta_oauth, true)) ? {} : local.labels.v2, try(each.value.labels, {})) : {}, # TODO make forward auth less static
-      tobool(try(each.value.okta_oauth, true)) ? {} : {
-        "traefik.http.routers.${lower(each.key)}.middlewares": "forward-auth",
+      tobool(try(each.value.okta_oauth, true)) ? { "traefik.http.routers.${lower(each.key)}.middlewares": "forward-auth" } : {
         "traefik.http.routers.${lower(each.key)}.rule" : "Host(${join(",", formatlist("`%s`", [for i in tolist(try(tolist(each.value.subdomains), [each.key])) : join(".", [i, local.domain])]))})",
         #"traefik.http.routers.${lower(each.key)}.service" : lower(each.key),
         "traefik.http.services.${lower(each.key)}.loadbalancer.server.port" : split(":", replace(try(tolist(each.value.ports), ["80:80"]).0, "/", ":")).1,
