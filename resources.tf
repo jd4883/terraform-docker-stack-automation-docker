0b7880e179d795a8e13e2e059b8597c6c293295b
Jacob Dresdale <jacob.dresdale@quantcast.com>
2021-05-29 10:24:31 -0700

    vpn logic is still off should be containers name for the router service not the vpn (though technically both hare not inherently wrong)

--- resources.tf
+++ resources.tf
@@ -81,7 +81,7 @@ resource "docker_container" "container" {
         "traefik.enable" : true,
         "traefik.http.routers.${lower(each.key)}.rule" : "Host(${join(",", formatlist("`%s`", [for i in tolist(try(tolist(each.value.subdomains), [each.key])) : join(".", [i, local.domain])]))})",
         "traefik.http.services.${lower(each.key)}.loadbalancer.server.port" : split(":", replace(try(tolist(each.value.ports), ["80:80"]).0, "/", ":")).1,
-        "traefik.http.routers.${lower(each.key)}.service" : try(each.value.networks.vpn, "") == "" ? lower(each.key) : each.value.networks.vpn,
+        "traefik.http.routers.${lower(each.key)}.service" : lower(each.key),
         #"traefik.http.middlewares.${lower(each.key)}.headers.sslhost" : join(",", formatlist("`%s`", [for i in tolist(try(tolist(each.value.subdomains), [each.key])) : join(".", [i, local.domain])])),
         #"traefik.http.middlewares.${lower(each.key)}-compression.compress" : tobool(try(each.value.compression, false)),
       }) : {},
