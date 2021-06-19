1685a9484b34fd36b1689432546d6e7eda1e18aa
Jacob Dresdale <15719920+jd4883@users.noreply.github.com>
2021-06-19 12:28:56 -0700

    a bit more cleanup, will prune old releases

--- submodules.tf
+++ submodules.tf
@@ -1,16 +1,16 @@
 module "dns" {
   for_each   = var.stack
-  cnames     = distinct(concat(each.value.subdomains, [each.key]))
+  cnames     = distinct(concat(lookup(each.value, "subdomains", []), [each.key]))
   domain     = local.domain
   private_ip = var.globals.networking.host_ip
   external_dns = tobool(try(each.value.external_dns, try(try(tomap(each.value.networks), { "frontend" : false }).frontend, false)))
   internal_dns = tobool(try(each.value.internal_dns, true))
 
-  logo_url              = tostring(try(each.value.logo_url, ""))
+  logo_url              = tostring(lookup(each.value, "logo_url", ""))
   name                  = lower(tostring(each.key))
-  okta_oauth            = tobool(try(each.value.okta_oauth, true))
+  okta_oauth            = tobool(lookup(each.value, "okta_oauth", true))
   source                = "jd4883/internal-external-dns-automation-docker/docker"
-  labels                = try(each.value.labels, {})
+  labels                = lookup(each.value, "labels", {})
   emails                = local.emails
   envars                = local.envars
   upstream_url          = "http://${try(each.value.networks.vpn, try(each.value.hostname, lower(each.key)))}:${split(":", replace(try(tolist(each.value.ports), ["80:80"]).0, "/", ":")).1}"
