b2cd63f5b67efccb7980115b30e774e230600e28
Condoriano <15719920+jd4883@users.noreply.github.com>
2021-06-19 17:43:38 -0700

    Delete secrets.tf

--- secrets.tf
+++
@@ -1,7 +0,0 @@
-//resource "random_string" "cookie_secret" {
-//  for_each = var.stack
-//  length   = var.cookie_secret_length
-//  special  = var.cookie_secret_special
-//}
-//
-//# TODO: this part probably only should happen for the fw_auth container and nowhere else
