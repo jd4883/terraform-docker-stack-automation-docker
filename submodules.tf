c17ead37e69b58e33fc60a96cfc9d7c872d9d4c6
Jacob Dresdale <15719920+jd4883@users.noreply.github.com>
2021-06-19 18:04:49 -0700

    testing without dns

--- submodules.tf
+++ submodules.tf
@@ -1,21 +1,20 @@
-module "dns" {
-  for_each     = var.stack
-  cnames       = distinct(concat(lookup(each.value, "subdomains", []), [each.key]))
-  domain       = local.domain
-  private_ip   = var.globals.networking.host_ip
-  external_dns = tobool(lookup(each.value, "external_dns", try(lookup(each.value, "networks", { "frontend" : false }).frontend, false)))
-  internal_dns = tobool(lookup(each.value, "internal_dns", true))
-
-  logo_url              = tostring(lookup(each.value, "logo_url", ""))
-  name                  = lower(tostring(each.key))
-  okta_oauth            = tobool(lookup(each.value, "okta_oauth", true))
-  source                = "jd4883/internal-external-dns-automation-docker/docker"
-  labels                = lookup(each.value, "labels", {})
-  emails                = local.emails
-  envars                = local.envars
-  upstream_url          = "http://${try(each.value.networks.vpn, lookup(each.value, "hostname", lower(each.key)))}:${split(":", replace(try(tolist(each.value.ports), ["80:80"]).0, "/", ":")).1}"
-  customResponseHeaders = var.customResponseHeaders
-  organizr_cname        = var.organizr_cname
-  STSSeconds            = var.STSSeconds
-  networks              = var.networks.*.id
-}
+//module "dns" {
+//  STSSeconds            = var.STSSeconds
+//  cnames                = distinct(concat(lookup(each.value, "subdomains", []), [each.key]))
+//  customResponseHeaders = var.customResponseHeaders
+//  domain                = local.domain
+//  emails                = local.emails
+//  envars                = local.envars
+//  external_dns          = tobool(lookup(each.value, "external_dns", try(lookup(each.value, "networks", { "frontend" : false }).frontend, false)))
+//  for_each              = var.stack
+//  internal_dns          = tobool(lookup(each.value, "internal_dns", true))
+//  labels                = lookup(each.value, "labels", {})
+//  logo_url              = tostring(lookup(each.value, "logo_url", ""))
+//  name                  = lower(tostring(each.key))
+//  networks              = var.networks.*.id
+//  okta_oauth            = tobool(lookup(each.value, "okta_oauth", true))
+//  organizr_cname        = var.organizr_cname
+//  private_ip            = var.globals.networking.host_ip
+//  source                = "jd4883/internal-external-dns-automation-docker/docker"
+//  upstream_url          = "http://${try(each.value.networks.vpn, lookup(each.value, "hostname", lower(each.key)))}:${split(":", replace(try(tolist(each.value.ports), ["80:80"]).0, "/", ":")).1}"
+//}
